4b025480269977aac30953e19f99aabc4f122f6e
trriplejay <johns@jfrog.com>
2019-05-23 14:13:05 -0700

    try script

---
+++ executeStuff.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "executing this script"
+echo "will cause a single command to have multiple children"
+wc -l "$STEP_JSON_PATH"
+cat "$STEP_JSON_PATH" | jq '.step'
+cat badfile
+echo "hello, we shouldn't make it this far"
