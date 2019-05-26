81b48849a84750f18be6ddd1d60ad36c83721fff
trriplejay <johns@jfrog.com>
2019-05-26 14:39:55 -0700

    try again

--- executeStuff.sh
+++ executeStuff.sh
@@ -5,4 +5,4 @@ echo "will cause a single command to have multiple children"
 wc -l "$STEP_JSON_PATH"
 cat "$STEP_JSON_PATH" | jq '.step'
 cat badfile
-echo "hello, we shouldn't make it this far, should we?"
+echo "hello, we shouldn't make it this far."
